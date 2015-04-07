7a009343c3a7047d058e407d2b9ebd5e55d218b9
ericyl <jhkd829511@gmail.com>
2015-04-07 19:15:16 +0800

    update

--- app/src/main/java/com/ericyl/themedemo/ui/activity/SettingActivity.java
+++ app/src/main/java/com/ericyl/themedemo/ui/activity/SettingActivity.java
@@ -1,20 +1,32 @@
 package com.ericyl.themedemo.ui.activity;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.ericyl.themedemo.R;
+import com.ericyl.themedemo.util.Settings;
 
 public class SettingActivity extends ActionBarActivity {
     private Toolbar toolbar;
+
+
+    private static final int REQ_CREATE_PATTERN = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         init();
+
+        Intent intent = new Intent(LockPatternActivity.ACTION_CREATE_PATTERN, null,
+                this, LockPatternActivity.class);
+        Settings.Security.setAutoSavePattern(this, true);
+        startActivityForResult(intent, REQ_CREATE_PATTERN);
+
     }
 
     private void init() {
